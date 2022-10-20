5507a3df7cf6c56f7a0019f8aa08c31d1e607678
Joel Dice <joel.dice@fermyon.com>
2022-10-20 08:06:51 -0600

    set outbound URL via env variable
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- src/main/java/wit_spin_http/SpinHttpImpl.java
+++ src/main/java/wit_spin_http/SpinHttpImpl.java
@@ -11,27 +11,26 @@ import wit_wasi_outbound_http.WasiOutboundHttp.Response;
 import wit_wasi_outbound_http.WasiOutboundHttp.Result;
 
 public class SpinHttpImpl {
-  private static final String URL = "https://some-random-api.ml/facts/dog";
-
   public static SpinHttp.Response handleHttpRequest(SpinHttp.Request request) {
+    String url = System.getenv("URL");
+
     Result<Response, HttpError> result =
         WasiOutboundHttp.request(
-            new Request(Method.GET, URL, new ArrayList<>(), new ArrayList<>(), null));
+            new Request(Method.GET, url, new ArrayList<>(), new ArrayList<>(), null));
 
     int status;
     String body;
     if (result.tag == Result.OK) {
       status = 200;
-
       Response response = result.getOk();
       if (response.status == (short) 200) {
         body = "Here's a dog fact: " + new String(response.body, UTF_8);
       } else {
-        body = "Unexpected response from " + URL + ": " + response.status;
+        body = "Unexpected response from " + url + ": " + response.status;
       }
     } else {
       status = 500;
-      body = "Trouble reaching " + URL + ": " + result.getErr();
+      body = "Trouble reaching " + url + ": " + result.getErr();
     }
 
     return new SpinHttp.Response((short) status, new ArrayList<>(), body.getBytes(UTF_8));
