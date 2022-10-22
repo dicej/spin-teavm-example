b81ee8483d1cd36fbe53f8c833b9a3c1a42b435a
Joel Dice <joel.dice@fermyon.com>
2022-10-21 19:49:48 -0600

    add static asset, switch outbound URL, update TeaVM-WASI dep
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- src/main/java/wit_spin_http/SpinHttpImpl.java
+++ src/main/java/wit_spin_http/SpinHttpImpl.java
@@ -2,6 +2,10 @@ package wit_spin_http;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import wit_wasi_outbound_http.WasiOutboundHttp;
 import wit_wasi_outbound_http.WasiOutboundHttp.HttpError;
@@ -11,28 +15,63 @@ import wit_wasi_outbound_http.WasiOutboundHttp.Response;
 import wit_wasi_outbound_http.WasiOutboundHttp.Result;
 
 public class SpinHttpImpl {
+    /**
+     * Interpret the word after the last '/' in the request URI as an English
+     * word and respond with any Spanish translations found for that word.
+     *
+     * If no word is specified, respond with text retrieved from the URL
+     * specified by the "URL" environment variable.
+     */
   public static SpinHttp.Response handleHttpRequest(SpinHttp.Request request) {
-    String url = System.getenv("URL");
+    int status = 404;
+    String body = "Disculpe, no entiendo.\n";
+    String contentType = "text/plain;charset=UTF-8";
 
-    Result<Response, HttpError> result =
-        WasiOutboundHttp.request(
-            new Request(Method.GET, url, new ArrayList<>(), new ArrayList<>(), null));
+    String query = request.uri.substring(request.uri.lastIndexOf('/') + 1);
+    if (query.isEmpty()) {
+      String url = System.getenv("URL");
 
-    int status;
-    String body;
-    if (result.tag == Result.OK) {
-      status = 200;
-      Response response = result.getOk();
-      if (response.status == (short) 200) {
-        body = "Here's a dog fact: " + new String(response.body, UTF_8);
+      Result<Response, HttpError> result =
+          WasiOutboundHttp.request(
+              new Request(Method.GET, url, new ArrayList<>(), new ArrayList<>(), null));
+
+      if (result.tag == Result.OK) {
+        status = 200;
+        Response response = result.getOk();
+        if (response.status == (short) 200) {
+          body = "Por tu consideración, un poema:\n\n" + new String(response.body, UTF_8) + "\n";
+        } else {
+          body = "Una respuesta inesperada: " + url + ": " + response.status + "\n";
+        }
       } else {
-        body = "Unexpected response from " + url + ": " + response.status;
+        status = 500;
+        body = "Problema con " + url + ": " + result.getErr() + "\n";
       }
     } else {
-      status = 500;
-      body = "Trouble reaching " + url + ": " + result.getErr();
+      try (BufferedReader reader =
+          new BufferedReader(new InputStreamReader(new FileInputStream("/en-es.csv"), UTF_8)); ) {
+        String line;
+        while ((line = reader.readLine()) != null) {
+          int comma = line.indexOf(",");
+          if (comma > 0 && line.substring(0, comma).equals(query)) {
+            status = 200;
+            body = line.substring(comma + 1) + "\n";
+            break;
+          }
+        }
+      } catch (IOException e) {
+        status = 500;
+        body = "No puedo leer el archivo: " + e + "\n";
+      }
     }
 
-    return new SpinHttp.Response((short) status, new ArrayList<>(), body.getBytes(UTF_8));
+    return new SpinHttp.Response(
+        (short) status,
+        new ArrayList<>() {
+          {
+            add(new SpinHttp.Tuple2<>("content-type", contentType));
+          }
+        },
+        body.getBytes(UTF_8));
   }
 }
